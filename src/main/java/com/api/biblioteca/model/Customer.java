e845f63bbb6a0ab23221195857256c90f18efb25
valeria-ferreira <ferreira_val@outlook.com>
2024-10-22 21:31:12 -0300

    Adicionado novos atributos e getters/setters na classe Customer

--- src/main/java/com/api/biblioteca/model/Customer.java
+++ src/main/java/com/api/biblioteca/model/Customer.java
@@ -1,6 +1,7 @@
 package com.api.biblioteca.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import jakarta.persistence.Entity; 
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;  
 
 @Entity
-@Table(name = "customers")  
+@Table(name = "customers") 
 public class Customer {
 
     @Id
@@ -17,7 +18,100 @@ public class Customer {
     private Long id;
 
     private String name;
+    private String lastname;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
     private LocalDate birthDate;
+    private Boolean isActive = true;
 
-    // Construtores, getters, setters etc.
+    // Getters e Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Customer other = (Customer) obj;
+        return Objects.equals(this.id, other.id);
+    }
 }
