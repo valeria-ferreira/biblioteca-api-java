9d91c7b56abeb4a03eeccf7ed57b0e596928109f
Valéria Ferreira <89273671+valeria-ferreira@users.noreply.github.com>
2024-10-24 19:16:20 -0300

    Update LoanRepository.java

--- src/main/java/com/api/biblioteca/repository/LoanRepository.java
+++ src/main/java/com/api/biblioteca/repository/LoanRepository.java
@@ -1,5 +1,12 @@
 package com.api.biblioteca.repository;
 
-public class LoanRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.biblioteca.model.Loan;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
     
 }
+
