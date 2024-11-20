a634af1d4037f931e8b7d82e0f3dca0ab7924c1e
IgorPinheiiro <igorpinheiro976@gmail.com>
2024-11-20 17:11:56 -0300

    Fix Swagger Customer

--- src/main/java/com/api/biblioteca/model/Customer.java
+++ src/main/java/com/api/biblioteca/model/Customer.java
@@ -2,15 +2,19 @@ package com.api.biblioteca.model;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.CascadeType;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "customers")
@@ -18,22 +22,38 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID do cliente", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
 
+    @Schema(description = "Nome do cliente", example = "João", required = true)
     private String nome;
+
+    @Schema(description = "Sobrenome do cliente", example = "Silva")
     private String sobrenome;
+
+    @Schema(description = "Endereço do cliente", example = "Rua 123, Bairro XYZ")
     private String endereco;
+
+    @Schema(description = "Cidade do cliente", example = "São Paulo")
     private String cidade;
+
+    @Schema(description = "Estado do cliente", example = "SP")
     private String estado;
+
+    @Schema(description = "País do cliente", example = "Brasil")
     private String pais;
+
+    @Schema(description = "Data de nascimento do cliente", example = "1990-12-15")
     private LocalDate dataNascimento;
 
     @Enumerated(EnumType.STRING)
+    @Schema(description = "Status do cliente", example = "ATIVO", allowableValues = {"ATIVO", "INATIVO"})
     private StatusCliente status;
 
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
-    private List<Loan> emprestimos; 
-
+    @JsonIgnore // Evita que a lista de empréstimos seja exibida quando o cliente for criado ou atualizado
+    @Schema(description = "Lista de empréstimos do cliente", hidden = true)  // Excluído do Swagger
+    private List<Loan> emprestimos;
     // Getters e Setters
 
     public Long getId() {
